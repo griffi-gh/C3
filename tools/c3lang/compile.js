9097e6dd548681298487e562be60005d09116b8e
griffi-gh <prasol258@gmail.com>
2021-11-05 17:21:18 +0200

    Update compile.js

--- tools/c3lang/compile.js
+++ tools/c3lang/compile.js
@@ -1,10 +1,34 @@
 function compileString(str) {
   let tokens = [];
   if(1){
-    let currentToken;
+    let tmp = '';
+    let parseStack = [];
+    let parseType = 'code';
+    let tokenInfo = {};
     for (var i = 0; i < str.length; i++) {
-      if(!currentToken) {
-        
+      let char = str.charAt(i);
+      switch(parseType) {
+        case 'code':
+          if(char=='"') {
+            parseStack.push('code');
+            parseType = 'str';
+            tmp = '';
+            continue;
+          }
+          break;
+        case 'str':
+          switch(char) {
+            case '"':
+              tokens.push({
+                type: 'str',
+                value: tmp
+              })
+              parseType = parseStack.pop();
+              continue;
+            default:
+              tmp += char;
+              break;
+          }
       }
     }
   }
