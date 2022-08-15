05f0c35e220b6556d0addc0c876f48df0508e2fa
griffi-gh <prasol258@gmail.com>
2022-08-15 16:24:17 +0200

    sync

--- tools/sim.bat
+++ tools/sim.bat
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 @REM simple script to redirect calls to Node.js
 @echo off
 setlocal
 rem cd /d "%~dp0/sim"
 node "%~dp0/sim" %*
+=======
+@REM simple script to redirect calls to Node.js
+@echo off
+setlocal
+cd /d "%~dp0/asm"
+node ./index.js "%1"
+>>>>>>> Stashed changes
